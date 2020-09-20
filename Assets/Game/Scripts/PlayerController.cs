860821c0d3b5cf5553f1508d2df61ec69e98a4a9
Huzaifa Saboowala <huzaifa.saboowala96@gmail.com>
2020-09-19 21:15:23 -0400

    Created seperate scene for bartender

---
+++ Assets/Game/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float movementX;
+    private float movementY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementX = Input.GetAxis("Horizontal");
+        movementY = Input.GetAxis("Vertical");
+
+        Debug.Log("X: " + movementX);
+        Debug.Log("Y: " + movementY);
+    }
+}
