57799cb89362d445907803a87b5d375b66fee78a
_Prashat <prashat@ymail.com>
2020-09-20 19:08:31 -0400

    Dialogs changed from sprites to Images

---
+++ Assets/Game/Scripts/Helpers/SpriteAdjust.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <author>
+//      Prashat Gajre [gajre@sheridancollege.ca]
+// </author>
+//-----------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteAdjust : MonoBehaviour
+{
+    RectTransform rect;
+    SpriteRenderer sprite;
+    void Start()
+    {
+        rect = GetComponent<RectTransform>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        sprite.size = new Vector2(rect.rect.width, rect.rect.height);
+    }
+}
