f89c0f68efc20e59ff9f8c5dcd149e380be7d7fa
_Prashat <prashat@ymail.com>
2020-09-21 19:21:55 -0400

    Dialog fix for server in and out of range.

--- Assets/Eavesdropper.cs
+++ Assets/Eavesdropper.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Eavesdropper : MonoBehaviour
 {
     public float m_minEavesdropDist = 2.0f;
-    private readonly List<PlayerDialog> playerDialogs = new List<PlayerDialog>();
-    
+    //private readonly List<PlayerDialog> playerDialogs = new List<PlayerDialog>();
+    private readonly List<CompanyNPC> npcsInRange = new List<CompanyNPC>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,23 +19,43 @@ public class Eavesdropper : MonoBehaviour
     {
         //Populate the PlayerDialogs list (This if code will execute only once, Did this here instead of start because CompanyNPCs add themselves on Start so
         //I was getting an empty list)
-        if(playerDialogs.Count <=0)
+        //if(playerDialogs.Count <=0)
+        //{
+        //    foreach (CompanyNPC npc in NPCManager.Instance.mTotalNPCs)
+        //    {
+        //        PlayerDialog playerDialog = npc.mPlayerDialog;
+        //        if (playerDialog != null)
+        //        {
+        //            playerDialogs.Add(playerDialog);
+        //        }
+        //    }
+        //}
+
+        //foreach (PlayerDialog npcDialog in playerDialogs)
+        //{
+        //    if (Vector3.Distance(this.transform.position, npcDialog.transform.root.position) <= m_minEavesdropDist)
+        //    {
+        //        npcDialog.MakeReadable();
+        //    }
+        //}
+
+        foreach (CompanyNPC npc in NPCManager.Instance.mTotalNPCs)
         {
-            foreach (CompanyNPC npc in NPCManager.Instance.mTotalNPCs)
+            if (Vector3.Distance(transform.position, npc.transform.position) <= m_minEavesdropDist)
             {
-                PlayerDialog playerDialog = npc.GetComponentInChildren<PlayerDialog>();
-                if (playerDialog != null)
+                if (!npcsInRange.Contains(npc))     //Makes sure that the trigger is called only once.
                 {
-                    playerDialogs.Add(playerDialog);
+                    npcsInRange.Add(npc);
+                    npc.mPlayerDialog.MakeReadable();
                 }
             }
-        }
-
-        foreach (PlayerDialog npcDialog in playerDialogs)
-        {
-            if (Vector3.Distance(this.transform.position, npcDialog.transform.root.position) <= m_minEavesdropDist)
-            {
-                npcDialog.MakeReadable();
+            else
+            { 
+                if (npcsInRange.Contains(npc))      //Makes sure that the list is cleated and triggers the dialog back to being unreadable.
+                {
+                    npcsInRange.Remove(npc);
+                    npc.mPlayerDialog.MakeUnreadable();
+                }
             }
         }
     }
