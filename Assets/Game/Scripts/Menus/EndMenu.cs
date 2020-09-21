85763af972e92925109ff0e0ffbe80e4de66d784
Huzaifa Saboowala <huzaifa.saboowala96@gmail.com>
2020-09-21 19:44:17 -0400

    Created a new menu for game end

---
+++ Assets/Game/Scripts/Menus/EndMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndMenu : Menu
+{
+   public GameObject m_mainMenuPanel;
+
+    public void BackToMainMenu()
+    {
+        gameObject.SetActive(false);
+        m_mainMenuPanel.SetActive(true);
+    }
+}
